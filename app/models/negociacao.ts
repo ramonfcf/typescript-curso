827aa2e4735487085995cc32136e85d8a2ff28e4
ramonfcf <ramonfcf@gmail.com>
2022-03-31 11:32:05 -0300

    typando métodos e parâmetros

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,27 @@
 export class Negociacao {
-   #data;
-   #quantidade;
-   #valor;
+   private _data: Date;
+   private _quantidade: number;
+   private _valor: number;
 
-   constructor(data, quantidade, valor) {
-      this.#data =  data;
-      this.#quantidade = quantidade;
-      this.#valor = valor;
+   constructor(data: Date, quantidade: number, valor: number) {
+      this._data =  data;
+      this._quantidade = quantidade;
+      this._valor = valor;
    }
 
-   get data() {
-      return this.#data;
+   get data(): Date {
+      return this._data;
    }
 
-   get quantidade() {
-      return this.#quantidade;
+   get quantidade(): number {
+      return this._quantidade;
    }
 
-   get valor() {
-      return this.#valor;
+   get valor(): number {
+      return this._valor;
    }
 
-   get volume() {
-      return this.#valor * this.#quantidade;
+   get volume(): number {
+      return this._valor * this._quantidade;
    }
 }
